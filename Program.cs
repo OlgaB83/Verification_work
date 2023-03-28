b5c2d71ebbcad91cc243afc20227bc5713e84141
Olga B <lelikm83@mail.ru>
2023-03-29 02:14:34 +0300

    добавление метода вывода заданного массиваа в  файл Programs.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
 ﻿string[] array = { "1222", "58", "море", "3", "2", "456", "7", "151", "155" };
+
+void PrintArray(string[] array)
+{
+       int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}" + ", ");
+    }
+
+}
+Console.Write("Заданный массив: ");
+PrintArray(array);
